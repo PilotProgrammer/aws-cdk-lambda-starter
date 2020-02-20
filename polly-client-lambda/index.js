f23571aff60d791ed8763042ec5963c345990b5b
garrett.granacher <pilot5698@yahoo.com>
2020-02-19 19:18:12 -0500

    cleanup

--- polly-client-lambda/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.handler = async (event = {}) => {
-    console.log('Hello World-js!');
-    const response = 'Hello World-js!' + JSON.stringify(event, null, 2);
-    return response;
-};
-
-console.log('whatup-thar-js!');
--- polly-client-lambda/src/run-handler.ts
+++
@@ -1,8 +0,0 @@
-import { Program, PollyClient } from './index'
-
-// const prog = new Program();
-// const response = prog.main({ "testKey": "testValue" });
-// console.log ('lambda response:', JSON.stringify(response))
-
-const polly = new PollyClient()
-polly.synth()
